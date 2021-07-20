abf69835dd4ff2895b742a28f4e8458811be5f6d
Werner Laurensse <werner@alman.ax>
2021-07-20 08:07:41 +0200

    Implement ext03

--- src/exterior.rs
+++ src/exterior.rs
@@ -1,5 +1,7 @@
 use core_simd::{f32x4};
 
+use crate::util::{dp};
+
 pub fn ext00(_a:f32x4,_b:f32x4)->(f32x4,f32x4) {
   todo!()
 }
@@ -8,8 +10,12 @@ pub fn ext02(_a:f32x4,_b:f32x4)->f32x4 {
   todo!()
 }
 
-pub fn ext03<const N:bool>(_a:f32x4,_b:f32x4)->f32x4 {
-  todo!()
+// p0 ^ p3 = -p3 ^ p0
+pub fn ext03<const F:bool>(a:f32x4, b:f32x4)->f32x4 {
+  // (a0 b0 + a1 b1 + a2 b2 + a3 b3) e0123
+  let mut p2 = dp(a,b);
+  if F { p2 = -p2 }
+  p2
 }
 
 pub fn extpb(_a:f32x4,_b:f32x4)->f32x4 {
