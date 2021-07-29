db85074c2884cd597841ddbf1e47b9ae87c68172
Werner Laurensse <werner@alman.ax>
2021-07-29 13:35:17 +0200

    Add partial tets for join operation

--- tests/join_test.rs
+++ tests/join_test.rs
@@ -1,16 +1,40 @@
+// Join Operation, Regressive Product, &
 #[cfg(test)]
 mod tests {
-  use g3::{plane,point};
+  use g3::{plane,line,point,ideal_line,branch};
+  #[test] fn join_point_point() {
+    let a = point(1.0, 2.0, 3.0);
+    let b = point(1.0, 2.0, 3.0);
+    let _l1 = a & b;
+    todo!();
+  }
+  #[test] fn join_point_line() {
+    let a = point(1.0, 2.0, 3.0);
+    let l = line(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+    let _p1 = a & l;
+    let _p2 = l & a;
+    todo!();
+  }
+  #[test] fn join_point_branch() {
+    let a = point(1.0, 2.0, 3.0);
+    let l = branch(1.0,2.0, 3.0);
+    let _p1 = a & l;
+    let _p2 = l & a;
+    todo!();
+  }
+  #[test] fn join_point_ideal_line() {
+    let a = point(1.0, 2.0, 3.0);
+    let l = ideal_line(1.0,2.0, 3.0);
+    let _p1 = a & l;
+    let _p2 = l & a;
+    todo!();
+  }
   #[test] fn join_plane_point() {
     let p1 = plane(1.0, 2.0, 3.0, 4.0);
     let a = point(1.0, 2.0, 3.0);
-    let _p2 = p1 & a;
+    let _d1 = p1 & a;
+    let _d2 = a & p1;
     todo!();
   }
-  #[test] fn join_point_point() {
-    todo!();
-  }
-  #[test] fn join_line_point() {
-    todo!();
-  }
+  
 }
