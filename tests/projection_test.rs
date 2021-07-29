17c0d6a8eb3344636110f892e3fb4db9550ed5b9
Werner Laurensse <werner@alman.ax>
2021-07-29 23:14:57 +0200

    Add test for project onto point

--- tests/projection_test.rs
+++ tests/projection_test.rs
@@ -3,6 +3,7 @@ mod tests {
   use g3::{plane,point,line};
 
   #[test] fn project_point_onto_line() { todo!(); }
+  #[test] fn project_plane_onto_line() { todo!(); }
   #[test] fn project_point_onto_plane() {
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let a = point(1.0, 2.0, 3.0);
@@ -15,7 +16,10 @@ mod tests {
     let _ = p.project_line(l);
     todo!()
   }
-  #[test] fn project_plane_onto_point() { todo!(); }
+  #[test] fn project_plane_onto_point() {
+    let p = plane(1.0, 2.0, 3.0, 4.0);
+    let a = point(1.0, 2.0, 3.0);
+    let _ = a.project_plane(p);
+  }
   #[test] fn project_line_onto_point() { todo!(); }
-  #[test] fn project_plane_onto_line() { todo!(); }
 }
