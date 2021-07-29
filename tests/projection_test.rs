2ebd1a75ccd63a28cddd1348d0c25e9f12f7a104
Werner Laurensse <werner@alman.ax>
2021-07-29 23:28:18 +0200

    Add project onto element tests

--- tests/projection_test.rs
+++ tests/projection_test.rs
@@ -2,8 +2,18 @@
 mod tests {
   use g3::{plane,point,line};
 
-  #[test] fn project_point_onto_line() { todo!(); }
-  #[test] fn project_plane_onto_line() { todo!(); }
+  #[test] fn project_point_onto_line() {
+    let l = line(1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
+    let a = point(1.0, 2.0, 3.0);
+    let _ = l.project_point(a);
+    todo!()
+  }
+  #[test] fn project_plane_onto_line() {
+    let l = line(1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
+    let p = plane(1.0, 2.0, 3.0, 4.0);
+    let _ = l.project_plane(p);
+    todo!()
+  }
   #[test] fn project_point_onto_plane() {
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let a = point(1.0, 2.0, 3.0);
@@ -20,6 +30,12 @@ mod tests {
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let a = point(1.0, 2.0, 3.0);
     let _ = a.project_plane(p);
+    todo!()
   }
-  #[test] fn project_line_onto_point() { todo!(); }
+  #[test] fn project_line_onto_point() {
+    let p = plane(1.0, 2.0, 3.0, 4.0);
+    let l = line(1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
+    let _ = p.project_line(l);
+    todo!()
+  }
 }
