a4e0b2b6fc37ef36b16fb6cd3a5df9470ed377df
Werner Laurensse <werner@alman.ax>
2021-07-31 16:51:54 +0200

    Test Plane.approx_eq

--- tests/plane_test.rs
+++ tests/plane_test.rs
@@ -9,6 +9,18 @@ mod tests {
     assert_eq!(plane(1.0,2.0,3.0,4.0), plane(1.0,2.0,3.0,4.0));
     assert_ne!(plane(1.0,2.0,3.0,4.0), plane(4.0,3.0,2.0,1.0));
   }
+  #[test] fn plane_approx_eq() {
+    let a  = plane(1.0, 1.0, 1.0, 1.0);
+    let b = plane(0.9, 0.9, 0.9, 0.9);
+    let c = plane(0.8, 0.8, 0.8, 0.8);
+    assert_eq!(a.approx_eq(b, 0.1001), true, "{:?} eq {:?} approx 0.1", a.p0, b.p0);
+    assert_eq!(a.approx_eq(b, 0.099), false, "{:?} eq {:?} approx 0.11", a.p0, b.p0);
+    assert_eq!(a.approx_eq(c, 0.1), false, "{:?} eq {:?} approx 0.09", a.p0, c.p0);
+    assert_eq!(a.approx_eq(c, 0.2), true, "{:?} eq {:?} approx 0.1", a.p0, c.p0);
+    let a1  = plane(1.0, 2.0, 3.0, 4.0);
+    let b1 = plane(0.9, 2.0, 3.0, 4.0);
+    assert_eq!(a1.approx_eq(b1, 0.1001), true, "{:?} eq {:?} approx 0.1", a1.p0, b1.p0);
+  }
   #[test] fn plane_getters() {
     let p = plane(4.0,2.0,3.0,1.0);
     assert_eq!(p.x(), 4.0);
@@ -18,7 +30,7 @@ mod tests {
     assert_eq!(p.e1(), 4.0);
     assert_eq!(p.e2(), 2.0);
     assert_eq!(p.e3(), 3.0);
-    assert_eq!(p.e0(), 1.0); 
+    assert_eq!(p.e0(), 1.0);
   }
   #[test] fn plane_reflect_plane() {
     let plane1 = Plane::new(1.0,2.0,3.0,4.0);
