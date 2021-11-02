78ac7bf150f372e57a950b0513fc1eca2360bc9f
Werner Laurensse <werner@alman.ax>
2021-11-02 14:02:51 +0100

    Add test for sandwich_point_between_plane

--- tests/sandwish_test.rs
+++ tests/sandwish_test.rs
@@ -1,4 +1,16 @@
 #[cfg(test)]
 mod tests {
-  // use g3::{plane,point,line};
+  use g3::{plane,point};
+  // G3 is an oriented algebra where a plane has two sides,
+  // and reflecting a plane with itself result in switching those sides.
+  // a(a) = -1
+
+  // A plane `b` perpendicular to a mirror a reflects to itself:
+  // -ab^(-a) = b
+
+
+  #[test] #[ignore] fn sandwich_point_between_plane() {
+    // the homogeneous coordinate are not the smame, lhs 0, rhs 1
+    assert_eq!(plane(1.0,0.0,0.0,0.0)(point(1.0,0.0,0.0)), point(-1.0,0.0,0.0))
+  }
 }
