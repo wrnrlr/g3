c676befd0c9da648ed4b0291e166c459b6a2acb0
Werner Laurensse <werner@alman.ax>
2021-11-10 22:52:36 +0100

    Test multiply_motor_by_scalar

--- tests/motor_test.rs
+++ tests/motor_test.rs
@@ -6,4 +6,9 @@ mod tests {
     let m = Motor::new(0.1,0.2,0.3,0.4,0.1,0.2,0.3,0.4).normalized();
     assert_eq!((m*m.reverse()).scalar(), 1.0, "for a normalized motor m*~m = 1")
   }
+
+  #[test] fn multiply_motor_by_scalar() {
+    let m = Motor::new(0.1,0.2,0.3,0.4,0.1,0.2,0.3,0.4)*2.0;
+    assert_eq!(m, Motor::new(0.2,0.4,0.6,0.8,0.2,0.4,0.6,0.8));
+  }
 }
