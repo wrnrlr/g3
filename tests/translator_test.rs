ed697962ab94eb10419fb2620de8fdd952c07bff
Werner Laurensse <werner@alman.ax>
2021-11-10 23:31:22 +0100

    Test Translator::new

--- tests/translator_test.rs
+++ tests/translator_test.rs
@@ -5,12 +5,18 @@
 
 #[cfg(test)]
 mod tests {
-  use g3::{Point};
+  use g3::{Point, Translator};
 
   #[test] fn translator_from_points() {
     let a = Point::new(1.0,1.0,1.0);
     let b = Point::new(-1.0,-1.0,-1.0);
     let t = (b.normalized() / a.normalized()).sqrt();
     assert_eq!(t(a), b);
+  }
+
+  #[test] fn translator_new() {
+    let origin = Point::new(0.0,0.0,0.0);
+    let t = Translator::new(4.0,1.0,0.0,1.0);
+    assert_eq!(t(origin), Point::new(8f32.sqrt(), 0.0, 8f32.sqrt()));
   }
 }
