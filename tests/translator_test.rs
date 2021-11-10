4d5b283ba7a834d46b666198256aba2c916d0917
Werner Laurensse <werner@alman.ax>
2021-11-10 22:58:11 +0100

    Test transformer from points

--- tests/translator_test.rs
+++ tests/translator_test.rs
@@ -3,3 +3,14 @@
 // 1 - (t2+t1)e/2 + t2et1e/4 = , because e^2 = 0
 // 1 - (t2+t1)e/2
 
+#[cfg(test)]
+mod tests {
+  use g3::{Point};
+
+  #[test] fn translator_from_points() {
+    let a = Point::new(1.0,1.0,1.0);
+    let b = Point::new(-1.0,-1.0,-1.0);
+    let t = (b.normalized() / a.normalized()).sqrt();
+    assert_eq!(t(a), b);
+  }
+}
