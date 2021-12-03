68e9bf173997dcf451ed00598c452fbbad862842
Werner Laurensse <werner@alman.ax>
2021-12-03 19:34:30 +0100

    Icosahedron Example

---
+++ examples/icosahedron.rs
@@ -0,0 +1,27 @@
+// Based on https://enkimute.github.io/ganja.js/examples/coffeeshop.html#pga3d_icosahedron
+
+use g3::{*};
+use baryon::{Color};
+
+fn main() {
+  let mut mr = Mirror::new();
+
+  let mut r = &mut Rotor::new(PI/2.5, 0.0, 1.0, 0.0);
+  let a = point(0.0,1.0,0.0);
+  let mut b = point((1.0 - 0.5f32.atan().powf(2.0)).sqrt(), 0.5f32.atan().atan(), 0.0);
+  let mut c = Rotor::new(PI/5.0, 0.0, 1.0, 0.0)(E2(b));
+
+  mr.vertex(a, Color::RED);
+  mr.vertex(b, Color::RED);
+  mr.vertex(c, Color::RED);
+
+  for _ in 0..5 {
+    b = r(b);
+    mr.vertex(b, Color::BLUE);
+    c = r(c);
+    mr.vertex(c, Color::BLUE);
+    mr.vertex(E2(a), Color::BLUE);
+  }
+
+  mr.run();
+}
