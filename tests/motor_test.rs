e4f142dfc50a970d7acdb6d228eb8bfbd73242fb
wrnrlr <werner@alman.ax>
2021-12-26 22:15:31 +0100

    Use point instead of Point::new in motor_test

--- tests/motor_test.rs
+++ tests/motor_test.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
   use std::f32::consts::PI;
-  use g3::{Motor, Rotor, Translator, Point};
+  use g3::{Motor, Rotor, Translator, Point, point};
 
   #[test] #[ignore] fn motor_normalized() {
     let m = Motor::new(0.1,0.2,0.3,0.4,0.1,0.2,0.3,0.4).normalized();
@@ -14,26 +14,26 @@ mod tests {
   }
 
   #[test] fn motor_from_translator() {
-    let a = Point::new(2.0,0.0,0.0);
+    let a = point(2.0,0.0,0.0);
     let m = Motor::from(Translator::new(2.0,1.0,0.0,0.0));
-    assert_eq!(m(a), Point::new(4.0, 0.0, 0.0));
+    assert_eq!(m(a), point(4.0, 0.0, 0.0));
   }
 
   #[test] fn motor_from_rotor() {
     // Rotate point 90 degrees
-    let a = Point::new(2.0,0.0,0.0);
-    let m = Motor::from(Rotor::new(-PI/2.0,0.0,0.0,1.0));
-    assert_eq!(m(a).normalized(), Point::new(0.0,2.0,0.0));
+    let a = point(2.0,0.0,0.0);
+    let m:Motor = Rotor::new(-PI/2.0,0.0,0.0,1.0).into();
+    assert_eq!(m(a).normalized(), point(0.0,2.0,0.0));
   }
 
   #[test] fn motor_product_of_rotor_translator() {
     // FIXME
     let r = Rotor::new(-PI/2.0,0.0,0.0,1.0);
     let t = Translator::new(2.0,1.0,0.0,0.0);
-    let a = Point::new(2.0,2.0,0.0);
+    let a = point(2.0,2.0,0.0);
     let m = r*t;
     assert_ne!((r*t)(a), (t*r)(a));
-    assert_eq!(m(a), Point::new(2.0,2.0,0.0));
+    assert_eq!(m(a), point(2.0,2.0,0.0));
     // assert_eq!(m(a), r(t(a)));
     // assert_eq!(m(a), t(r(a)));
   }
