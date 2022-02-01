e1a6c7fd0c9c2a99171c4a7696df7947ff36e22c
wrnrlr <werner@alman.ax>
2022-02-01 18:52:30 +0100

    Ignore some old tests

--- tests/line_test.rs
+++ tests/line_test.rs
@@ -15,28 +15,28 @@ mod tests {
   #[test] fn line_getters() {
     let _l1 = line(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
   }
-  #[test] fn line_add() {
+  #[test] #[ignore] fn line_add() {
     todo!()
   }
-  #[test] fn line_add_assign() {
+  #[test] #[ignore] fn line_add_assign() {
     todo!()
   }
-  #[test] fn line_sub() {
+  #[test] #[ignore] fn line_sub() {
     todo!()
   }
-  #[test] fn line_sub_assign() {
+  #[test] #[ignore] fn line_sub_assign() {
     todo!()
   }
-  #[test] fn line_mul_scalar() {
+  #[test] #[ignore] fn line_mul_scalar() {
     todo!()
   }
-  #[test] fn line_mul_assign_scalar() {
+  #[test] #[ignore] fn line_mul_assign_scalar() {
     todo!()
   }
-  #[test] fn line_div_scalar() {
+  #[test] #[ignore] fn line_div_scalar() {
     todo!()
   }
-  #[test] fn line_div_assign_scalar() {
+  #[test] #[ignore] fn line_div_assign_scalar() {
     todo!()
   }
   #[test] fn line_dual() {
--- tests/projection_test.rs
+++ tests/projection_test.rs
@@ -2,37 +2,37 @@
 mod tests {
   use g3::{plane,point,line};
 
-  #[test] fn project_point_onto_line() {
+  #[test] #[ignore] fn project_point_onto_line() {
     let l = line(1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
     let a = point(1.0, 2.0, 3.0);
     let _ = l.project_point(a);
     todo!()
   }
-  #[test] fn project_plane_onto_line() {
+  #[test] #[ignore] fn project_plane_onto_line() {
     let l = line(1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let _ = l.project_plane(p);
     todo!()
   }
-  #[test] fn project_point_onto_plane() {
+  #[test] #[ignore] fn project_point_onto_plane() {
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let a = point(1.0, 2.0, 3.0);
     let _ = p.project_point(a);
     todo!()
   }
-  #[test] fn project_line_onto_plane() {
+  #[test] #[ignore] fn project_line_onto_plane() {
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let l = line(1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
     let _ = p.project_line(l);
     todo!()
   }
-  #[test] fn project_plane_onto_point() {
+  #[test] #[ignore] fn project_plane_onto_point() {
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let a = point(1.0, 2.0, 3.0);
     let _ = a.project_plane(p);
     todo!()
   }
-  #[test] fn project_line_onto_point() {
+  #[test] #[ignore] fn project_line_onto_point() {
     let p = plane(1.0, 2.0, 3.0, 4.0);
     let l = line(1.0, 2.0, 3.0, 1.0, 2.0, 3.0);
     let _ = p.project_line(l);
