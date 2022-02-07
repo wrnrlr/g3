149644499655b7e073be6fca8990ca476805450d
wrnrlr <werner@alman.ax>
2022-02-07 22:32:54 +0100

    repl: Use unary and binary expressions in AST

--- examples/repl.rs
+++ examples/repl.rs
@@ -1,6 +1,6 @@
 #[allow(unused_imports)]
 use g3::{point,line,plane,Point,Line,Plane};
-use peg::parser;
+use peg::{parser};
 
 parser! {
   grammar algebra() for str {
@@ -9,79 +9,91 @@ parser! {
 
     rule _ = [' ' | '\n']*
 
+    rule number() -> Expression
+        = n:$("-"?['0'..='9']+("."['0'..='9']+)?) { Expression::Number(n.parse().unwrap()) }
+
+    rule symbol() -> Expression
+        = s:$(['a'..='z' | 'A'..='Z']['a'..='z' | 'A'..='Z' | '0'..='9']*) { Expression::Symbol(s.parse().unwrap()) }
+
+    rule list() -> Expression
+        = "{" s:expression() ** "," "}" { Expression::List(Box::new(s)) }
+
+    rule sequence() -> Sequence
+        = s:expression() ** "," { Sequence(s) }
+
     rule sum() -> Expression
-        = l:product() _ "+" _ r:product() { Expression::Sum(Box::new(l), Box::new(r)) }
+        = l:minus() _ "+" _ r:minus() { Expression::Binary(Operator::Plus, Box::new(l), Box::new(r)) }
+        / minus()
+
+    rule minus() -> Expression
+        = l:negative() _ "-" _ r:negative() { Expression::Binary(Operator::Minus, Box::new(l), Box::new(r)) }
+        / negative()
+
+    rule negative() -> Expression
+        = "-" _ r:product() { Expression::Unary(Operator::Minus, Box::new(r)) }
         / product()
 
     rule product() -> Expression
-        = l:number() r:atom() { Expression::Product(Box::new(l), Box::new(r)) }
-        / l:atom() _ "*"? _ r:atom() { Expression::Product(Box::new(l), Box::new(r)) }
-        / atom()
+        = l:number() r:atom() { Expression::Binary(Operator::Multiply, Box::new(l), Box::new(r)) }
+        / l:atom() _ "*"? _ r:atom() { Expression::Binary(Operator::Multiply, Box::new(l), Box::new(r)) }
+        / division()
+
+    rule division() -> Expression
+        = l:atom() _ "/" _ r:atom() { Expression::Binary(Operator::Divide, Box::new(l), Box::new(r)) }
+        / call()
 
     rule call() -> Expression
-        = l:symbol() "[" r:expression() ** "," "]" { Expression::Call(Box::new(l), Box::new(r)) }
+        = h:symbol() "[" s:expression() ** "," "]" { Expression::Call(Box::new(h), Box::new(s)) }
+        / atom()
+
+    rule assignment() -> Expression
+        = l:symbol() "=" r:expression() { Expression::Binary(Operator::Assign, Box::new(l), Box::new(r)) }
 
     rule atom() -> Expression
         = symbol()
         / number()
         / list()
-        / "(" _ v:sum() _ ")" { v }
-    
-    rule number() -> Expression
-        = n:$(['0'..='9']+("."['0'..='9']+)?) { Expression::Number(n.parse().unwrap()) }
-    
-    rule symbol() -> Expression
-        = s:$(['a'..='z' | 'A'..='Z']['a'..='z' | 'A'..='Z' | '0'..='9']*) { Expression::Symbol(s.parse().unwrap()) }
-    
-    rule list() -> Expression
-        = "{" l:expression() ** "," "}" { Expression::List(Box::new(l)) }
+        / "(" _ v:expression() _ ")" { v }
   }
 }
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum Operator {
+  Plus, Minus, Negative, Multiply, Divide, Power, Assign
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Sequence(Vec<Expression>);
 
 #[derive(Clone, PartialEq, Debug)]
 pub enum Expression {
   Number(f32),
   Symbol(String),
   List(Box<Vec<Expression>>),
-  Sum(Box<Expression>,Box<Expression>),
-  Product(Box<Expression>,Box<Expression>),
+  Binary(Operator, Box<Expression>,Box<Expression>),
+  Unary(Operator, Box<Expression>),
   Call(Box<Expression>, Box<Vec<Expression>>)
 }
 // https://corywalker.me/2018/06/03/introduction-to-computer-algebra.html
 fn main() {
   println!("g3 repl");
   assert_eq!(algebra::expression("4"), Ok(Expression::Number(4.0)));
-  assert_eq!(algebra::expression("1.2"), Ok(Expression::Number(1.2)));
-  assert_eq!(algebra::expression("a"), Ok(Expression::Symbol("a".to_string())));
-  assert_eq!(algebra::expression("Abc"), Ok(Expression::Symbol("Abc".to_string())));
-  assert_eq!(algebra::expression("e01"), Ok(Expression::Symbol("e01".to_string())));
-  assert_eq!(algebra::expression("3e01"), Ok(Expression::Product(
-    Box::new(Expression::Number(3.0)), Box::new(Expression::Symbol("e01".to_string())))));
-  assert_eq!(algebra::expression("4.0 e01"), Ok(Expression::Product(
-    Box::new(Expression::Number(4.0)), Box::new(Expression::Symbol("e01".to_string())))));
-  assert_eq!(algebra::expression("a2 b3"), Ok(Expression::Product(
-    Box::new(Expression::Symbol("a2".to_string())), Box::new(Expression::Symbol("b3".to_string())))));
-  assert_eq!(algebra::expression("1+1"), Ok(Expression::Sum(
-    Box::new(Expression::Number(1.0)), Box::new(Expression::Number(1.0)))));
-  assert_eq!(algebra::expression("5*5"), Ok(Expression::Product(
-      Box::new(Expression::Number(5.0)), Box::new(Expression::Number(5.0)))));
-  assert_eq!(algebra::expression("2+3*4"), Ok(Expression::Sum(
-      Box::new(Expression::Number(2.0)),
-      Box::new(Expression::Product(
-          Box::new(Expression::Number(3.0)),
-          Box::new(Expression::Number(4.0)))))));
-  assert_eq!(algebra::expression("(2+3)*4"), Ok(Expression::Product(
-      Box::new(Expression::Sum(
-          Box::new(Expression::Number(2.0)),
-          Box::new(Expression::Number(3.0)))),
-      Box::new(Expression::Number(4.0)))));
-  assert_eq!(algebra::expression("{}"), Ok(Expression::List(Box::new(vec![]))));
-  assert_eq!(algebra::expression("{1,a,b+2.0}"), Ok(Expression::List(
-    Box::new(vec!(Expression::Number(1.0), Expression::Symbol("a".to_string()), Expression::Sum(
-      Box::new(Expression::Symbol("b".to_string())),
-      Box::new(Expression::Number(2.0))))))));
-  assert_eq!(algebra::expression("f[]"), Ok(
-    Expression::Call(
-      Box::new(Expression::Symbol("f".to_string())),
-      Box::new(Vec::new()))));
+  assert!(algebra::expression("-1").is_ok());
+  assert!(algebra::expression("1.2").is_ok());
+  assert!(algebra::expression("a").is_ok());
+  assert!(algebra::expression("Abc").is_ok());
+  assert!(algebra::expression("e01").is_ok());
+  assert!(algebra::expression("3e01").is_ok());
+  assert!(algebra::expression("4.0 e01").is_ok());
+  assert!(algebra::expression("a2 b3").is_ok());
+  assert!(algebra::expression("1+1").is_ok());
+  assert!(algebra::expression("5*5").is_ok());
+  assert!(algebra::expression("2+3*4").is_ok());
+  assert!(algebra::expression("(2+3)*4").is_ok());
+  assert!(algebra::expression("1*-2").is_ok());
+  assert!(algebra::expression("-2*a").is_ok());
+  assert!(algebra::expression("{}").is_ok());
+  assert!(algebra::expression("{1,a,b+2.0}").is_ok());
+  assert!(algebra::expression("f[]").is_ok());
+  assert!(algebra::expression("f[1,a]").is_ok());
 }
