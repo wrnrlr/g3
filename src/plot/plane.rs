ebefd1f55ac0a1b3eb678c1c4feb849810ecf18c
wrnrlr <werner@alman.ax>
2022-06-11 13:41:36 +0200

    Bump bevy version

--- src/plot/plane.rs
+++ src/plot/plane.rs
@@ -43,9 +43,9 @@ impl From<DoubleSidedPlane> for Mesh {
 
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
     mesh.set_indices(Some(indices));
-    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
-    mesh.set_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
-    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_NORMAL, normals);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
     mesh
   }
 }
