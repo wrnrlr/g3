01fb4b1d6827d949e454e0bcda4e47ed7e2e7d74
wrnrlr <werner@alman.ax>
2022-06-15 01:30:35 +0200

    Fix plane mesh

--- src/mirror/mod.rs
+++ src/mirror/mod.rs
@@ -2,21 +2,15 @@ use glam::Vec3;
 use rend3::types::{Handedness, Mesh, MeshBuilder};
 use crate::{Plane,E1,E2,E3,point};
 
-fn vertex(pos: [f32; 3]) -> glam::Vec3 {
-  glam::Vec3::from(pos)
-}
-
 pub fn create_plane_mesh(p:Plane)-> Mesh {
   let p = p.normalized();
-  // for i in [[-1,-1],[-1,1],[1,1],[-1,-1],[1,1],[1,-1]] {}
-  let m = (p*E1).sqrt();
-  // x*o.Length,e0,z*o.Length,1
-  // let points = [[-1f32,-1.0],[-1.0,1.0],[1.0,1.0],[-1.0,-1.0],[1.0,1.0],[1.0,-1.0]].map(|i|{point(i.0,0.0,i.1)});
+  let m = (p*E2).sqrt();
   let a = m(point(-1.0,0.0,-1.0));
   let b = m(point(-1.0,0.0,1.0));
   let c = m(point(1.0,0.0,1.0));
   let d = m(point(1.0,0.0,-1.0));
   let vertices:Vec<Vec3> = vec!(a.into(), b.into(), c.into(), d.into());
+  // let vertices:Vec<Vec3> = vec!(a, b, c, d).into();
   let indices = vec!(0u32, 2, 1, 0, 3, 2, 2, 3, 0, 1, 2, 0);
 
   // normals??
