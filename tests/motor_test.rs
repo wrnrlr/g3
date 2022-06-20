83ffb9b683165cd13556e92f33b852da55d869d4
wrnrlr <werner@alman.ax>
2022-06-20 21:06:43 +0200

    Fix use

--- tests/motor_test.rs
+++ tests/motor_test.rs
@@ -1,7 +1,6 @@
 #[cfg(test)]
 mod tests {
-  use std::f32::consts::PI;
-  use g3::{Motor, Rotor, Translator, point};
+  use g3::{Motor, Rotor, Translator, point, PI};
 
   #[test] #[ignore] fn motor_normalized() {
     let m = Motor::new(0.1,0.2,0.3,0.4,0.1,0.2,0.3,0.4).normalized();
