d44d94c7ef3aa2355a61948430d9de3061e6c131
wrnrlr <werner@alman.ax>
2022-06-22 08:52:58 +0200

    Move rotor test

--- src/rotor.rs
+++ src/rotor.rs
@@ -308,3 +308,17 @@ impl Div<Motor> for Rotor {
     self * m.inverse()
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::Rotor;
+
+  #[test] fn rotor_constrained() {
+    let r1 = Rotor::new(1.0, 2.0, 3.0, 4.0);
+    let r2 = r1.constrained();
+    assert_eq!(r1, r2);
+    let r3 = -r1;
+    let r4 = r1.constrained();
+    assert_eq!(r3, -r4);
+  }
+}
