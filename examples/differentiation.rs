d041873010d7b83538ec3a642f8e475c60cea64d
wrnrlr <werner@alman.ax>
2022-06-26 02:37:00 +0200

    Refactor differentiation.rs

--- examples/differentiation.rs
+++ examples/differentiation.rs
@@ -1,4 +1,4 @@
-use g3::*;
+use g3::prelude::*;
 
 fn align(p:Point, q:Point)->Translator {
   (q.normalized()/p.normalized()).sqrt()
@@ -16,8 +16,9 @@ fn path(m:Translator, n:u32, x:Point)->impl Iterator<Item = Point> {
 const N:u32 = 8;
 
 fn main() {
-  App::new().add_plugin(PlotPlugin)
-    .add_startup_system(|mut cmd: Commands| {
+  App::new()
+    .add_plugin(PlotPlugin)
+    .add_startup_system(|mut cmd: Cmd| {
       let (a,b,d) = (point(-1.0,1.0,0.0), point(1.0,1.0,0.0), point(-1.0,-1.0,0.0));
       let ad = align(a,d);
       for i in 0..N {
@@ -27,15 +28,13 @@ fn main() {
         let pq = align(down(a), q);
         path(pq, N, p).for_each(|p|{cmd.spawn_bundle((p, Color::CYAN));});
       }})
-    .add_system(play)
+    .add_system(|time: Res<Time>, mut q:Q<(&mut Point, &Color)>|{
+      let t = time.seconds_since_startup() as f32;
+      for (mut p,_) in q.iter_mut() {
+        let x = p.x(); let y = p.y();
+        let z = 0.5*(t*5.0).sin()*x*x*x-0.5*t.cos()*y*y;
+        *p = point(x, y, z);
+      }
+    })
     .run();
-}
-
-fn play(time: Res<Time>, mut q:Query<(&mut Point, &Color)>) {
-  let t = time.seconds_since_startup() as f32;
-  for (mut p,_) in q.iter_mut() {
-    let x = p.x(); let y = p.y();
-    let z = 0.5*(t*5.0).sin()*x*x*x-0.5*t.cos()*y*y;
-    *p = point(x, y, z);
-  }
 }
